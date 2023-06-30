f5a7de486d2f1f5f879e72e1a8513f07747d2df5
Khuranes44441 <perezernesto523@gmail.com>
2023-06-30 16:49:21 -0300

    modificacion final para entregar

--- codigo.js
+++ codigo.js
@@ -5,6 +5,7 @@ let materia = [];
 let cicloLectivo = [];
 let notaParcial1 = [];
 let notaParcial2 = [];
+let notaTrabajoPractico = [];
 let promedioGeneral = [];
 
 Cargar();
@@ -20,6 +21,7 @@ function Cargar() {
     let c = prompt("Ciclo lectivo:");
     let np1 = parseFloat(prompt("Nota parcial 1:"));
     let np2 = parseFloat(prompt("Nota parcial 2:"));
+    let ntp = parseFloat(prompt("Nota trabajo practico:"));
     let promGen = (np1 + np2) / 2;
 
     matricula.push(m);
@@ -29,6 +31,7 @@ function Cargar() {
     cicloLectivo.push(parseInt(c));
     notaParcial1.push(np1);
     notaParcial2.push(np2);
+    notaTrabajoPractico.push(ntp);
     promedioGeneral.push(promGen);
   }
 
@@ -40,22 +43,49 @@ function Listar() {
 
   for (let i = 0; i < nombre.length; i++) {
     tbody.innerHTML += `
-      <tr>
-        <td>${matricula[i]}</td>
-        <td>${nombre[i]}</td>
-        <td>${apellido[i]}</td>
-        <td>${materia[i]}</td>
-        <td>${cicloLectivo[i]}</td>
-        <td>${notaParcial1[i]}</td>
-        <td>${notaParcial2[i]}</td>
-        <td>${promedioGeneral[i]}</td>
-      </tr>
-    `;
+        <tr>
+          <td>${matricula[i]}</td>
+          <td>${nombre[i]}</td>
+          <td>${apellido[i]}</td>
+          <td>${materia[i]}</td>
+          <td>${cicloLectivo[i]}</td>
+          <td>${notaParcial1[i]}</td>
+          <td>${notaParcial2[i]}</td>
+          <td>${notaTrabajoPractico[i]}</td>
+          <td>${promedioGeneral[i]}</td>
+        </tr>
+      `;
   }// ... Tu función Listar existente ...
 }
 
+
+function CalcularCantidadAlumnosPorCiclo(ciclo) {
+  let cantidadAlumnos = 0;
+  for (let i = 0; i < cicloLectivo.length; i++) {
+    if (cicloLectivo[i] === ciclo) {
+      cantidadAlumnos++;
+    }
+  }
+  const estadisticasElement = document.getElementById("estadisticas");
+  estadisticasElement.innerHTML += `Cantidad de alumnos en el ciclo lectivo ${ciclo}: ${cantidadAlumnos}<br>`;
+}
+
+
+// Función para calcular la cantidad de alumnos por materia
+function CalcularCantidadAlumnosPorMateria(nombreMateria) {
+  let cantidadAlumnos = 0;
+  for (let i = 0; i < materia.length; i++) {
+    if (materia[i] === nombreMateria) {
+      cantidadAlumnos++;
+    }
+  }
+  return cantidadAlumnos;
+}
+
+
+// Función para calcular todas las estadísticas
 function CalcularEstadisticas() {
-  let cont = nombre.length;
+  // ... Tlet cont = nombre.length;
   let materiasDesaprobadas = {};
   let materiasAprobadas = [];
   let materiasPromocionadas = [];
@@ -80,35 +110,37 @@ function CalcularEstadisticas() {
     }
   }
 
-  const estadisticasElement = document.getElementById("estadisticas");
-  estadisticasElement.innerHTML = "MATERIAS DESAPROBADAS:<br>";
+  const estadisticasElemento = document.getElementById("estadisticas");
+  estadisticasElemento.innerHTML = "MATERIAS DESAPROBADAS: <br>";
   for (let materiaDesaprobada in materiasDesaprobadas) {
-    estadisticasElement.innerHTML += `${materiasDesaprobadas[materiaDesaprobada]} alumno(s) desaprobó ${materiaDesaprobada}<br>`;
+    estadisticasElemento.innerHTML += `${materiasDesaprobadas[materiaDesaprobada]} alumno(s) desaprobó ${materiaDesaprobada}<br>`;
   }
 
-  estadisticasElement.innerHTML += `<br>MATERIAS APROBADAS: ${materiasAprobadas.join(", ")}<br>`;
-  estadisticasElement.innerHTML += `MATERIAS PROMOCIONADAS: ${materiasPromocionadas.join(", ")}<br>`;// ... Tu función CalcularEstadisticas existente ...
+  estadisticasElemento.innerHTML += `MATERIAS APROBADAS: ${materiasAprobadas.join(", ")}<br>`;
+  estadisticasElemento.innerHTML += `MATERIAS PROMOCIONADAS: ${materiasPromocionadas.join(", ")}<br>`;// ... Tu función CalcularEstadisticas existente ...
 
-    // Obtener los ciclos únicos del arreglo cicloLectivo
-    const ciclosUnicos = [...new Set(cicloLectivo)];
+  // Obtener los ciclos únicos del arreglo cicloLectivo
+  const ciclosUnicos = [...new Set(cicloLectivo)];
 
-    // Calcular y mostrar la cantidad de alumnos por ciclo
-    for (const ciclo of ciclosUnicos) {
-        CalcularCantidadAlumnosPorCiclo(ciclo);
-    }
+  // Calcular y mostrar la cantidad de alumnos por ciclo
+  for (const ciclo of ciclosUnicos) {
+    CalcularCantidadAlumnosPorCiclo(ciclo);
+  }
+
+  // Obtener las materias únicas del arreglo materia
+  const materiasUnicas = [...new Set(materia)];
+
+  // Calcular y mostrar la cantidad de alumnos por materia
+  const estadisticasElement = document.getElementById("estadisticas");
+  estadisticasElemento.innerHTML += "<br>Cantidad de alumnos por materia: <br>";
+  for (const materiaUnica of materiasUnicas) {
+    const cantidadAlumnosPorMateria = CalcularCantidadAlumnosPorMateria(materiaUnica);
+    estadisticasElemento.innerHTML += `${cantidadAlumnosPorMateria} alumno(s) en la materia ${materiaUnica}<br>`;
+  }
 }
 
-function CalcularCantidadAlumnosPorCiclo(ciclo) {
-    let cantidadAlumnos = 0;
-    for (let i = 0; i < cicloLectivo.length; i++) {
-        if (cicloLectivo[i] === ciclo) {
-            cantidadAlumnos++;
-        }
-    }
-    const estadisticasElement = document.getElementById("estadisticas");
-    estadisticasElement.innerHTML += `<br>Cantidad de alumnos en el ciclo lectivo ${ciclo}: ${cantidadAlumnos}`;
-}
-
+// Llamar a la función CalcularEstadisticas para que calcule todas las estadísticas
+CalcularEstadisticas();
 
 
 
